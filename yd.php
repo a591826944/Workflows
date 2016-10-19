f9d506eb02e65a17cb7548c2613efb336a2f95f6
wwpeng <591826944@qq.com>
2016-10-19 19:12:17 +0800

    有道字典 支持翻译

--- yd.php
+++ yd.php
@@ -3,13 +3,28 @@ require_once('workflows.php');
 require_once('ParserDom.php');
 $wf = new Workflows();
 $query = "{query}";
-$request = file_get_contents('http://dict.youdao.com/search?q='.$query.'&keyfrom=dict.top');
+$query = str_replace('\\ ', ' ', trim($query));
+$request = file_get_contents('http://dict.youdao.com/search?q='.urlencode($query).'&keyfrom=dict.top');
 $html_dom = new \HtmlParser\ParserDom($request);
-$result = $html_dom->find('#phrsListTab .trans-container ul li');
-foreach ($result as $value) 
+
+if(strstr($query, ' '))
 {
-	$value = explode('.', $value->getPlainText());
-	$type = trim(array_shift($value)," ");
-	$wf->result( $query, $query, $query.' - '.$type.'.', trim(implode(' ', $value)," "), 'icon.png' );
+    $result = $html_dom->find('#fanyiToggle .trans-container p');
+    if(count($result) >= 3)
+    {
+        $result = $result[1];
+        $wf->result( $query, $query, $query, trim($result->getPlainText()), 'icon.png' );
+    }
+
+}else{
+
+    $result = $html_dom->find('#phrsListTab .trans-container ul li');
+    foreach ($result as $value)
+    {
+        $value = explode('.', $value->getPlainText());
+        $type = trim(array_shift($value)," ");
+        $wf->result( $query, $query, $query.' - '.$type.'.', trim(implode(' ', $value)," "), 'icon.png' );
+    }
 }
+
 echo $wf->toxml();
